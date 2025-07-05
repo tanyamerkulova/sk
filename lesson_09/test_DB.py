f969ef6a3fa319e89ae06ff11275345c09c2c51c
tanyamerkulova <43956765+tanyamerkulova@users.noreply.github.com>
2025-07-05 14:06:34 +0300

    Добавлен assert

--- lesson_09/test_DB.py
+++ lesson_09/test_DB.py
@@ -15,22 +15,19 @@ db = create_engine(db_connection_string)
 # Проверка подключения и получения списка таблиц в базе
 def test_db_connection():
     inspector = inspect(db)  # Создаем инспектор для получения метаданных о БД
-    # Получаем список названий всех таблиц в базе
-    names = inspector.get_table_names()
-    # Проверяем, что вторая таблица называется 'subject'
-    assert names[1] == 'subject'
-
+    names = inspector.get_table_names()  # Получаем список названий всех таблиц в базе
+    assert names[1] == 'subject'  # Проверяем, что вторая таблица называется 'subject'
 
 # Выполнение запроса select * from student
 def test_select():
     connection = db.connect()  # Устанавливаем соединение с базой данных
-    result = connection.execute(text("SELECT * FROM student"))
+    result = connection.execute(text("SELECT * FROM student"))  # Выполняем SQL-запрос
 
-    # Получаем все строки как список словарей (ключ — имя столбца)
-    rows = result.mappings().all()
+    # Получаем все строки как список словарей (ключ — имя столбца) 
+    rows = result.mappings().all()  
     row1 = rows[0]  # Получаем первую строку из результата
 
-    # Проверка
+    # Проверка 
     assert row1['user_id'] == 42568
     assert row1['level'] == "Pre-Intermediate"
 
@@ -40,15 +37,20 @@ def test_select():
 # Функция для вставки новой строки в таблицу
 def test_insert():
     connection = db.connect()  # Устанавливаем соединение с базой данных
-    # Начинаем транзакцию (важно для операций изменения данных)
-    transaction = connection.begin()
+    transaction = connection.begin()  # Начинаем транзакцию (важно для операций изменения данных)
 
     sql = text("INSERT INTO subject(\"subject_title\") VALUES (:new_name)")
-
+    
     # Выполняем запрос, передавая значение параметра :new_name
     connection.execute(sql, {"new_name": "Chemistry"})
 
-    transaction.commit()
+    transaction.commit()  # Подтверждаем транзакцию, чтобы изменения сохранились в базе
+
+    result = connection.execute(text("SELECT * FROM subject WHERE subject_title = :new_name"),
+                                {"new_name": "Chemistry"}).mappings().first()
+    
+    assert result is not None
+    assert result["subject_title"] == "Chemistry"
 
     connection.close()  # Закрываем соединение
 
@@ -57,11 +59,16 @@ def test_update():
     connection = db.connect()  # Устанавливаем соединение с базой данных
     transaction = connection.begin()  # Начинаем транзакцию
 
-    sql = text("UPDATE subject SET"
-               " subject_title = :sub WHERE subject_id = :id")
+    sql = text("UPDATE subject SET subject_title = :sub WHERE subject_id = :id")
     connection.execute(sql, {"sub": "New sub", "id": 1})
 
     transaction.commit()  # Подтверждаем изменения
+    
+    result = connection.execute(text("SELECT subject_title FROM subject WHERE subject_id = :id"),
+                                {"id": 1}).mappings().first()
+    assert result is not None
+    assert result["subject_title"] == "New sub"
+    
     connection.close()  # Закрываем соединение
 
 
@@ -73,4 +80,10 @@ def test_delete():
     connection.execute(sql,  {"new_name": "Chemistry"})
 
     transaction.commit()
+
+    result = connection.execute(text("SELECT * FROM subject WHERE subject_title = :new_name"),
+                                {"new_name": "Chemistry"}).mappings().first()
+    
+    assert result is None  # Запись должна отсутствовать
     connection.close()
+
